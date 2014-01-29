a5e089feb4a8f3acb919ebd11926b273979cbed1
Matt Senn <eternalmatt@gmail.com>
2014-01-29 15:23:16 -0500

    New views

---
+++ app/controllers/leagues.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose')
+  , async = require('async')
+  , _ = require('underscore')
+  , League = mongoose.model('League')
+  ;
+
+exports.create = function(req, res){
+  var league = new League(req.body);
+  league.commissioner = req.user;
+  league.save();
+  res.jsonp(league);
+};
+
+exports.show = function(req, res){
+  res.jsonp(req.league);
+};
+
+exports.league = function(req, res, next, id){
+  var League = mongoose.model('League');
+  League.load(id, function(err, league){
+    if (err) return next(err);
+    if (!league) return next(new Error('Failed to load league' + id));
+    
+    req.league = league;
+    next();    
+  });
+};
+
+exports.all = function(req, res){
+  League.find().populate('commissioner').exec(function(err, leagues){
+    if (err){ res.render('error', {status: 500});}
+    else {
+      res.jsonp(leagues);
+    }
+  }); 
+};
+
+exports.update = function(req, res){
+  var league = req.league;
+  league = _.extend(league, req.body);
+  leage.save(function(err){
+    res.jsonp(league);
+  });
+};
+
+exports.destroy = function(req, res){
+  var league = req.league;
+  league.remove(function(err){
+    if (err){ res.render('error', {status: 500}); }
+    else {
+      res.jsonp(1);
+    }
+  });
+};
