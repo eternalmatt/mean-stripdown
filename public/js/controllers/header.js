c3dcfc4946adddb1f01cc114c570e14de0640790
Matt Senn <eternalmatt@gmail.com>
2014-01-29 13:29:46 -0500

    Different head if signed in

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,5 +1,4 @@
 window.angular.module('ngff.controllers.header', [])
-  .controller('HeaderController', [
-    function() {
-
-    }]);
+  .controller('HeaderController', function($scope, Global) {
+    $scope.global = Global;           
+  });
