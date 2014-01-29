29f6362534bd0a1fc66c8783cb96208280499121
Matt Senn <eternalmatt@gmail.com>
2014-01-29 14:44:39 -0500

    League Angular service

---
+++ public/js/services/leagues.js
@@ -0,0 +1,7 @@
+window.angular.module('ngff.services.leagues', [])
+  .factory('Leagues', function($resource){
+    return $resource('leagues/:leagueId', 
+      { leagueId: '@_id' },
+      { update: { method: 'PUT' } }
+    );
+  });
