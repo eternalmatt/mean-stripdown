1ca5be10f81e11dd0331a28a88e20a065f2500fa
Matt Senn <eternalmatt@gmail.com>
2014-01-31 23:22:53 -0500

    Two new directives for choosing player data

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,5 +3,19 @@ window.angular.module('ngff.directives', [])
     return {
       restrict: 'E',
       templateUrl: 'views/players/positionselect.html'
+    };
+  })
+
+  .directive('nflteams', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'views/players/nflteamselect.html'
+    };
+  })
+  
+  .directive('searchlimit', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'views/players/searchlimitselect.html'
     }
   })
