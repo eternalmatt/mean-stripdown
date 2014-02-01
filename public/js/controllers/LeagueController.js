c9adededdfe2b7d1201318ed440344d44fb05d2a
Matt Senn <eternalmatt@gmail.com>
2014-01-31 22:23:47 -0500

    Added basic error text...just in case

--- public/js/controllers/LeagueController.js
+++ public/js/controllers/LeagueController.js
@@ -5,6 +5,9 @@ window.angular.module('ngff.controllers.league', [])
     $scope.save = function(league){
       save(league).then(function(league){
         $location.path('leagues/' + league._id);
+      }, function error(e){
+        console.log(e);
+        $scope.errorText = "Error recording the league to database";
       });
     };
     
